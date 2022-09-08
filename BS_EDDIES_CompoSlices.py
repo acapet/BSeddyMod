999d0322e80e74bd992b11eb176450e776528c88
Arthur <arthurcapet@gmail.com>
2022-09-08 07:45:13 +0200

    U,V and rotation

--- BS_EDDIES_CompoSlices.py
+++ BS_EDDIES_CompoSlices.py
@@ -5,55 +5,44 @@ import netCDF4
 import xarray as xr
 from glob import glob
 
-runyear = "????"
+import matplotlib.dates as mdates
+old_epoch = "0000-12-31T00:00:00"
+mdates.set_epoch(old_epoch)
 
-directory_Z   = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/Z_slices/"
-directory_RHO = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/RHO_slices/"
+runyear = "2014"
+
+directory_Z   = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/Z_slices2/"
+directory_RHO = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/RHO_slices2/"
 files_Z       = sorted(glob(directory_Z + "BS_Z_slices_ACYC_track-????_"+runyear+"????_T.nc"))
 files_RHO     = sorted(glob(directory_RHO + "BS_RHO_slices_ACYC_track-????_"+runyear+"????_T.nc"))
 
-outdir        = directory_Z
-outfname      = outdir+'CompoRotated_%s_Region_%s_%s.nc' ## % Z/RHO regionint aggtype
+outdir        = directory_Z+'/../CompoSlices/'
+outfname      = outdir+'CompoRotated_%s_Region_%s_%s_%s.nc' ## % Z/RHO regionint aggtype
+
+seasonfilter  =  'ALLYEAR' # 'SUMMER' #
+
 
 for region in range(5):
     print('Processing Region %s'%region)
 
     print(' .. Z files')
     # Z FILES
-    xas=[]
-    for i, f in enumerate(files_Z):
-        xa=xr.load_dataset(f)
-        if i%100==0: print(i,f,xa.region.values[0])
-        if int(xa.region.values[0]) != region:
-            continue
-        #if initdone:
-        #    xas = [xa.drop(['xcoord','ycoord','index'])]
-        #else:
-        xas.append(xa.drop(['xcoord','ycoord','index']))
-
-    xm=xr.concat(xas,'index')
-
-    xmean  = xm.mean ('index')
-    xstd   = xm.std  ('index')
-    xcount = xm.count('index')
-
-    xmean.to_netcdf( outfname %('Z', str(region),'mean'))
-    xstd.to_netcdf(  outfname %('Z', str(region),'std'))
-    xcount.to_netcdf(outfname %('Z', str(region),'count'))
-
-    print(' .. RHO files')
-
-# RHO FILES
-    if False: 
+    if True:
         xas=[]
-        for i, f in enumerate(files_RHO):
-            print(i,f,xa.region.values[0])
+        for i, f in enumerate(files_Z):
+            
             xa=xr.load_dataset(f)
             if int(xa.region.values[0]) != region:
                 continue
-            #if i==0:
-            #    xas = [xa.drop(['xcoord','ycoord','index'])]
-            #else:
+            # Get date
+            ddate= mdates.num2date(xa.time.data)[0]
+            if i%100==0:
+                print(i,f,xa.region.values[0])
+            if (seasonfilter == 'SUMMER') & (ddate.month not in [4,5,6,7,8,9,10]):
+#                print( 'month %s discarded'%ddate.month)
+                continue
+#            else:
+#                print( 'month %s is kept '%ddate.month)
             xas.append(xa.drop(['xcoord','ycoord','index']))
 
         xm=xr.concat(xas,'index')
@@ -62,6 +51,53 @@ for region in range(5):
         xstd   = xm.std  ('index')
         xcount = xm.count('index')
 
-        xmean.to_netcdf(outfname %('RHO', str(region),'mean'))
-        xstd.to_netcdf(outfname %('RHO', str(region),'std'))
-        xcount.to_netcdf(outfname %('RHO', str(region),'count'))
+        xmean.to_netcdf( outfname %('Z', str(region),'mean' ,seasonfilter))
+        xstd.to_netcdf(  outfname %('Z', str(region),'std'  ,seasonfilter))
+        xcount.to_netcdf(outfname %('Z', str(region),'count',seasonfilter))
+
+        xmedian  = xm.median ('index')
+        xmedian.to_netcdf(outfname %('Z', str(region),'median',seasonfilter))
+        
+        xp25    = xm.quantile(.25,'index')
+        xp25.to_netcdf(outfname %('Z', str(region),'p25',seasonfilter))
+        
+        xp75  = xm.quantile (.75,'index')
+        xp75.to_netcdf(outfname %('Z', str(region),'p75',seasonfilter))
+
+
+    print(' .. RHO files')
+
+    # RHO FILES
+    if False: 
+        xas=[]
+        for i, f in enumerate(files_RHO):
+            xa=xr.load_dataset(f)
+            if i%100==0: print(i,f,xa.region.values[0])
+            if int(xa.region.values[0]) != region:
+                continue
+            # Get date
+            ddate= mdates.num2date(xa.time.data)[0]
+            if i%100==0:
+                print(i,f,xa.region.values[0])
+                print(ddate.month)
+            if (seasonfilter == 'SUMMER') & (ddate.month not in [4,5,6,7,8,9,10]):
+                continue
+            xas.append(xa.where((xa.rho>=12)&(xa.rho<=18), drop=True).drop(['xcoord','ycoord','index']))
+
+        xm=xr.concat(xas,'index')
+
+        xmean  = xm.mean ('index')
+        xmean.to_netcdf(outfname %('RHO', str(region),'mean',seasonfilter))
+
+        xstd   = xm.std  ('index')
+        xstd.to_netcdf(outfname %('RHO', str(region),'std',seasonfilter))
+
+        xmedian  = xm.median ('index')
+        xmedian.to_netcdf(outfname %('RHO', str(region),'median',seasonfilter))
+        xp25    = xm.quantile(.25,'index')
+        xp25.to_netcdf(outfname %('RHO', str(region),'p25',seasonfilter))
+        xp75  = xm.quantile (.75,'index')
+        xp75.to_netcdf(outfname %('RHO', str(region),'p75',seasonfilter))
+
+        xcount = xm.count('index')
+        xcount.to_netcdf(outfname %('RHO', str(region),'count',seasonfilter))
--- BS_EDDIES_correct_topo.py
+++ BS_EDDIES_correct_topo.py
@@ -1,11 +1,15 @@
 # %run make_BS_correct_1d_topo.py
 
-from numpy import array
+from numpy import array, deg2rad
 import xarray as xr
 from matplotlib import pyplot as plt
 from glob import glob
 from netCDF4 import Dataset
 import argparse
+import pyproj
+import math
+
+geodesic = pyproj.Geod(ellps='WGS84')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-y", "--year" , type=str, help="start year")
@@ -13,26 +17,66 @@ parser.add_argument("-y", "--year" , type=str, help="start year")
 args = parser.parse_args()
 runyear  = args.year
 
+def completemyd(dd,dtype):
+    dd['BGC']      = dd['NPPO']      - dd['bac_oxygenconsumption']  - dd['OXIDATIONBYDOX']   - dd['ZooResp']
+    dd['BGC_P']    = dd['NPPO_P']    - dd['bac_oxygenconsumption_P']- dd['OXIDATIONBYDOX_P'] - dd['ZooResp_P']
+
+    dd['Resp']     = dd['ZooResp']   + dd['bac_oxygenconsumption']
+    dd['Resp_P']   = dd['ZooResp_P'] + dd['bac_oxygenconsumption_P']
+
+    dd['DOXODU']   = dd['DOX']       - dd['ODU']
+    dd['DOXODU_P'] = dd['DOX_P']     - dd['ODU_P']
+    
+    if (dtype=='z'):
+        dd['BGCI']      = dd['NPPOI']      - dd['bac_oxygenconsumptionI']  - dd['OXIDATIONBYDOXI']   - dd['ZooRespI']
+        dd['BGCI_P']    = dd['NPPOI_P']    - dd['bac_oxygenconsumptionI_P']- dd['OXIDATIONBYDOXI_P'] - dd['ZooRespI_P'] 
+        dd['NetI']      = dd['BGCI']       + dd['AirSeaOxygenFlux'] 
+        dd['NetI_P']    = dd['BGCI_P']     + dd['AirSeaOxygenFlux_P']
+    return(dd)
+
+
+var3Dlonely = ['vort','w','u','v']
+var2Dlonely = ['mld','lon','lat','topo','ssh',]
+
+varphy3Dlist=['rho','temp','salt']
+varbio3Dlist=['NPPO','ZooResp','DOC','bac_oxygenconsumption','OXIDATIONBYDOX','CHL', 'DOX', 'NOS', 'POC', 'PAR', 'NHS', 'ODU']
+varbio2Dlist=['bac_oxygenconsumptionI', 'ZooRespI', 'NPPOI', 'OXIDATIONBYDOXI', 'AirSeaOxygenFlux']
+
+def flipmeifneeded(xl, flip,ztype):
+
+    if flip:
+        if ztype=='z':
+            for v in var2Dlonely:
+                xl[v][:] = xl[v].values[:, ::-1]
+            for v in varbio2Dlist:
+                xl[v][:] = xl[v].values[:,  ::-1]
+                xl[v+"_P"][:] = xl[v+'_P'].values[:, ::-1]
+        for v in var3Dlonely:
+            xl[v][:] = xl[v].values[:, :, ::-1]
+        for v in varphy3Dlist:
+            if (v=='rho') & (ztype=='rho'):
+                continue
+            xl[v][:] = xl[v].values[:, :, ::-1]
+            xl[v+"P"][:] = xl[v+'P'].values[:, :, ::-1]
+        for v in varbio3Dlist:
+            xl[v][:] = xl[v].values[:, :, ::-1]
+            xl[v+"_P"][:] = xl[v+'_P'].values[:, :, ::-1]
+           
+    return(xl)
+
 if __name__ == "__main__":
 
     plt.close("all")
 
     #savedir = "/Users/emason/Dropbox/BlackSea/figures_BS/"
     
-    directory_Z = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/Z_slices/"
-    directory_RHO = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/RHO_slices/"
+    directory_Z = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/Z_slices2/"
+    directory_RHO = "/home/arthur/Desktop/PAPERS_UNDER_WORK/Evan1/diagfiles/RHO_slices2/"
     filedir='../files/'    
     files_Z = sorted(glob(directory_Z + "BS_Z_slices_ACYC_track-????_"+runyear+"????.nc"))
     files_RHO = sorted(glob(directory_RHO + "BS_RHO_slices_ACYC_track-????_"+runyear+"????.nc"))
 
-    var3Dlonely = ['vort','w']
-    var2Dlonely = ['mld','lon','lat','topo','ssh',]
-
-    varphy3Dlist=['rho','temp','salt']
-    varbio3Dlist=['NPPO','ZooResp','DOC','bac_oxygenconsumption','OXIDATIONBYDOX','CHL', 'DOX', 'NOS', 'POC', 'PAR', 'NHS', 'ODU']
-    varbio2Dlist=['bac_oxygenconsumptionI', 'ZooRespI', 'NPPOI', 'OXIDATIONBYDOXI', 'AirSeaOxygenFlux']
            
-
     ######  For region assignement  ################################
     xreg  = xr.load_dataset(filedir+'../files/region_REVISITED.nc')
     xgrid = xr.load_dataset(filedir+'../files/mesh_mask_31levels.nc')
@@ -45,7 +89,7 @@ if __name__ == "__main__":
         ds_Z = xr.open_dataset(file_Z) 
 
         ######  For region assignement  ################################
-        newreg=xreg.region.interp({'x':ds_Z.centlon, 'y':ds_Z.centlat}).values
+        newreg=xreg.region.interp({'x':ds_Z.centlon, 'y':ds_Z.centlat}, method='nearest').values
         #################################################################
 
         topo = ds_Z.topo[0].values
@@ -58,56 +102,36 @@ if __name__ == "__main__":
             print("Need to flip %s"%file_Z)
             flip = True
 
-        if flip:
-            with Dataset(file_Z) as nc:
-                for v in var2Dlonely:
-                    ds_Z[v][:] = nc.variables[v][:, ::-1]
-                for v in var3Dlonely:
-                    ds_Z[v][:] = nc.variables[v][:, :, ::-1]
-                for v in varphy3Dlist:
-                    ds_Z[v][:] = nc.variables[v][:, :, ::-1]
-                    ds_Z[v+"P"][:] = nc.variables[v+'P'][:, :, ::-1]
-                for v in varbio3Dlist:
-                    ds_Z[v][:] = nc.variables[v][:, :, ::-1]
-                    ds_Z[v+"_P"][:] = nc.variables[v+'_P'][:, :, ::-1]
-                for v in varbio2Dlist:
-                    ds_Z[v][:] = nc.variables[v][:,  ::-1]
-                    ds_Z[v+"_P"][:] = nc.variables[v+'_P'][:, ::-1]               
+        ds_Z= flipmeifneeded(ds_Z,flip,'z')
 
-            ds_Z['region']=('one',newreg)
-            ds_Z.to_netcdf(files_Z[i].replace(".nc", "_T.nc"), unlimited_dims='index', format="NETCDF4")
-            ds_Z.close()
-            
+        ## 
+        lon_away = ds_Z.lon.interp({'norm_eddy_radius':2.5}).values
+        lat_away = ds_Z.lat.interp({'norm_eddy_radius':2.5}).values
+        lon_cen  = ds_Z.centlon.values
+        lat_cen  = ds_Z.centlat.values
+   
+        fwd_azimuth,back_azimuth,distance = geodesic.inv(lon_cen,lat_cen,lon_away,lat_away)
+        fwd_azimuth=fwd_azimuth[0]
+
+        ds_Z['v_across']= ds_Z['u']*math.cos(deg2rad(fwd_azimuth)) - ds_Z['v']*math.sin(deg2rad(fwd_azimuth))
+        ds_Z['v_along'] = ds_Z['u']*math.sin(deg2rad(fwd_azimuth)) + ds_Z['v']*math.cos(deg2rad(fwd_azimuth))
+        ##
+
+        ds_Z['region']=('one',newreg)
+        ds_Z = completemyd (ds_Z,'z')
+        ds_Z.to_netcdf(files_Z[i].replace(".nc", "_T.nc"), unlimited_dims='index', format="NETCDF4")
+        ds_Z.close()
+
+        if True:    
             ds_RHO = xr.open_dataset(files_RHO[i])
-            
-            with Dataset(files_RHO[i]) as nc:
-                for v in var3Dlonely:
-                    ds_RHO[v][:] = nc.variables[v][:, :, ::-1]
-                for v in varphy3Dlist:
-                    if v == 'rho':
-                        continue
-                    ds_RHO[v][:] = nc.variables[v][:, :, ::-1]
-                    ds_RHO[v+"P"][:] = nc.variables[v+'P'][:, :, ::-1]
-                for v in varbio3Dlist:
-                    ds_RHO[v][:] = nc.variables[v][:, :, ::-1]
-                    ds_RHO[v+"_P"][:] = nc.variables[v+'_P'][:, :, ::-1]
-#                for v in varbio2Dlist:
-#                    ds_RHO[v][:] = nc.variables[v][:,  ::-1]
-#                    ds_RHO[v+"_P"][:] = nc.variables[v+'_P'][:, ::-1]             
-            
+                
+            ds_RHO= flipmeifneeded(ds_RHO,flip,'rho')
+
+            ds_RHO['v_across']= ds_RHO['u']*math.cos(deg2rad(fwd_azimuth)) - ds_RHO['v']*math.sin(deg2rad(fwd_azimuth))
+            ds_RHO['v_along'] = ds_RHO['u']*math.sin(deg2rad(fwd_azimuth)) + ds_RHO['v']*math.cos(deg2rad(fwd_azimuth))
+
             ds_RHO['region']=('one',newreg)
-            ds_RHO.to_netcdf(files_RHO[i].replace(".nc", "_T.nc"), unlimited_dims='index', format="NETCDF4")
-            ds_RHO.close()
-        #sasa
-        
-        else:
-            #file_Z = file_Z.replace(".nc", "_T.nc")
-            ds_Z['region']=('one',newreg)
-            ds_Z.to_netcdf(files_Z[i].replace(".nc", "_T.nc"), unlimited_dims='index', format="NETCDF4")
-            ds_Z.close()
-            
-            #file_RHO = files_RHO[i].replace(".nc", "_T.nc")
-            ds_RHO['region']=('one',newreg)
+            ds_RHO = completemyd (ds_RHO,'rho')
             ds_RHO.to_netcdf(files_RHO[i].replace(".nc", "_T.nc"), unlimited_dims='index', format="NETCDF4")
             ds_RHO.close()
         
